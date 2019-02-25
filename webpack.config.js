0ef983bd9040805d2f3c424bf11c9d575a660824
KushibikiMashu <kushibiki3413@gmail.com>
2019-02-25 23:13:08 +0900

    Delete unused files

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,6 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
           },
-          // {
-          //   loader: 'style-loader',
-          // },
           {
             loader: 'css-loader',
             options: {
